9495d7273d67e29cce01ca63f702e373ad2b5ecf
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-01-07 11:54:58 +0630

    footer

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='text-sm mt-40 text-gray-600 text-center p-4 border border-t-gray-100'>
+      Copyright &copy; {new Date().getFullYear()} ZweL
+    </footer>
+  );
+}
