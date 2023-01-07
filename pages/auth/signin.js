ce0d3a22f58f16d2b7bda5983bcabcdf4ce8e84c
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-01-07 10:33:58 +0630

    google auth

---
+++ pages/auth/signin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+export default function Signin() {
+  return (
+    <>
+      <Header />
+      <div></div>
+    </>
+  );
+}
+
+export async function getServerSideProps() {}
