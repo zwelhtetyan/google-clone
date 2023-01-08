f58472fbc396a8053900d81c94c9d4511e0cbd09
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-01-08 11:40:02 +0630

    fetching data logic and use mockup data for test

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import SearchHeader from '../components/SearchHeader';
 import SearchResult from '../components/SearchResult';
+import { DUMMY_DATA_RESULT } from '../DUMMY_DATA.JS';
 
-export default function search() {
+export default function search({ searchedResult }) {
+  console.log(searchedResult);
+
   return (
     <>
       <Head>
@@ -16,4 +19,21 @@ export default function search() {
       <SearchResult />
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const searchQuery = context.query.q || '';
+  const searchType = context.query.searchType || '';
+
+  // const response = await fetch(
+  //   `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${
+  //     process.env.CX_KEY
+  //   }&q=${searchQuery}${searchType ? '&searchType=image' : ''}`
+  // );
+
+  // const searchedResult = await response.json();
+
+  // return { props: { searchedResult } };
+
+  return { props: { searchedResult: DUMMY_DATA_RESULT } };
 }
