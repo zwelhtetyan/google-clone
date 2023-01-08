0ed5fba5b3d9e9995f3c2f07f7df52e030c3f778
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-01-08 17:16:22 +0630

    change header tab transition

--- components/HeaderTab.jsx
+++ components/HeaderTab.jsx
@@ -16,7 +16,7 @@ export default function HeaderTab({ Icon, tabName, isActive }) {
       onClick={handleTabChange}
       className={`${
         isActive ? 'text-blue-500 border-b-blue-500' : 'border-b-transparent'
-      } flex space-x-1 cursor-pointer group pb-1 hover:text-blue-500 border-b-4 hover:border-b-blue-500 duration-150`}
+      } flex space-x-1 cursor-pointer group pb-1 hover:text-blue-500 border-b-4 hover:border-b-blue-500 duration-100`}
     >
       <Icon isActive={isActive} />
       <p>{tabName}</p>
